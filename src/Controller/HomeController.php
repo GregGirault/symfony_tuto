25ba099d2a1a4f13778c34d7e420847774e0774f
greg0788 <g.girault@codeur.online>
2023-08-03 11:24:53 +0200

    creation base de données

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController{
+
+    public function bonjour()
+    {
+
+        return new Response("bonjour à toutes et à tous");
+
+    }
+
+    public function aurevoir()
+    {
+       return $this->redirectToRoute('accueil');
+    }
+
+    public function redirectToLinkedIn()
+    {
+        return $this->redirect('https://www.linkedin.com/');
+    }
+
+    public function showtemplate()
+    {
+      return $this->render('base.html.twig', []);
+    }
+
+}
